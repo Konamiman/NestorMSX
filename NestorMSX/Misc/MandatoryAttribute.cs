2616f3127220cc9a22eb091c2426e2b8ebc4ea3c
Nestor Soriano Vilchez <nestor.soriano@sunhotels.net>
2015-01-13 14:01:06 +0100

    Project created from the last commit of the Z80dotNet solution, with the Z80dotNet project references changed to references to the NuGet package.

---
+++ NestorMSX/Misc/MandatoryAttribute.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Konamiman.NestorMSX.Misc
+{
+    /// <summary>
+    /// Attribute used to signal a string parameter
+    /// in the Configuration class as mandatory.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property)]
+    public class MandatoryAttribute : Attribute
+    {
+    }
+}
