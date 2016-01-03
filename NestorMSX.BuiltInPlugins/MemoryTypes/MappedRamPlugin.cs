7e771a1ffa719986c454590ac0f1937b1ff8fb92
Néstor Soriano <nestor.soriano@sunhotels.net>
2016-01-03 20:36:15 +0100

    Fixed a bug in the mapped RAM plugin. Now Nextor boots!!

--- NestorMSX.BuiltInPlugins/MemoryTypes/MappedRamPlugin.cs
+++ NestorMSX.BuiltInPlugins/MemoryTypes/MappedRamPlugin.cs
@@ -10,16 +10,33 @@ namespace Konamiman.NestorMSX.BuiltInPlugins.MemoryTypes
     public class MappedRamPlugin
     {
         private readonly int sizeInSegments;
+        private MappedRam mappedRam;
 
         public MappedRamPlugin(PluginContext context, IDictionary<string, object> pluginConfig)
         {
             var sizeInKb = pluginConfig.GetValue<int>("sizeInKb");
             sizeInSegments = sizeInKb/16;
+
+            this.mappedRam = new MappedRam(sizeInSegments);
+
+            context.Cpu.MemoryAccess += Cpu_MemoryAccess;
+        }
+
+        private void Cpu_MemoryAccess(object sender, MemoryAccessEventArgs e)
+        {
+            if(e.EventType != MemoryAccessEventType.BeforePortWrite)
+                return;
+
+            if(e.Address < 0xFC || e.Address > 0xFF)
+                return;
+
+            var page = e.Address - 0xFC;
+            mappedRam.WriteToSegmentSelectionRegister(page, e.Value);
         }
 
         public IMemory GetMemory()
         {
-            return new MappedRam(sizeInSegments);
+            return mappedRam;
         }
     }
 }
